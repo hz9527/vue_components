0f375aececd7d07d9c2efda6b8058eec5caed452
hz9527 <981379316@qq.com>
2017-04-26 21:19:37 +0800

    nothing

---
+++ src/components/picker/picker/hook.js
@@ -0,0 +1,20 @@
+// module.exports = (function(){
+//   if (!Object.keys) {
+//     Object.prototype.keys = function (obj) {
+//       var result = []
+//       for (var k in obj) {
+//         result.push(k)
+//       }
+//       return result
+//     }
+//   }
+//   if (!Object.values) {
+//     Object.prototype.keys = function (obj) {
+//       var result = []
+//       for (var k in obj) {
+//         result.push(obj[k])
+//       }
+//       return result
+//     }
+//   }
+// }())
