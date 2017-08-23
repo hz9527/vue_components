fb044a90658151ffabb5a94e07adc3167b8a1a48
hz9527 <981379316@qq.com>
2017-08-23 22:15:59 +0800

    laji

--- src/components/newSwiper/index.vue
+++ src/components/newSwiper/index.vue
@@ -1,5 +1,9 @@
 <template lang="html">
   <div class="swiper">
+    <div class="tips">
+      <slot name='other'></slot>
+      <slot name='tips'></slot>
+    </div>
     <div class="list" :style="{'width': length * 100 + '%'}" ref='list'>
       <slot name='list'></slot>
     </div>
@@ -17,10 +21,6 @@ export default {
     index: {
       type: Number,
       default: 0
-    },
-    speed: {
-      type: Number,
-      default: 50
     }
   },
   mounted () {
@@ -35,42 +35,64 @@ export default {
       step: this._width,
       max: 0,
       min: -this._width * (this.length - 1),
-      change: this.change
+      change: this.change,
+      animationEnd: this.animateEnd
     })
-    this._alloyTouch.to(-this.curIndex * this._width, 0)
+    this._alloyTouch.to(-this._curIndex * this._width, 0)
   },
   data () {
     return {
       _alloyTouch: null,
       _width: 0,
-      curIndex: 0
+      _curIndex: 0
     }
   },
   watch: {
     index: {
       immediate: true,
       handler (v, ov) {
-        this.curIndex = v
+        if (v !== this._curIndex) {
+          this._curIndex = v
+          if (this._alloyTouch) {
+            this._alloyTouch.to(-this._curIndex * this._width, 100)
+          }
+        }
       }
+    },
+    length (v) {
+      // set min
     }
   },
   methods: {
     change (v) {
-      // if (v > this.index *) {
-      //   console.log(-v / this._width)
-      // }
+      if (-v > (this.index + 0.6) * this._width) {
+        this._curIndex++
+        this.$emit('change', this.index + 1)
+      } else if (-v < (this.index - 0.6) * this._width) {
+        this._curIndex--
+        this.$emit('change', this.index - 1)
+      }
+    },
+    animateEnd (v) {
+      this._curIndex = -v / this._width
+      this.$emit('end', this._curIndex)
     }
   }
 }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 .swiper {
   width: 100%;
   height: 100%;
   overflow: hidden;
+  position: relative;
+}
+.tips {
+  height: 20%;
+  display: flex;
 }
 .list {
-  height: 100%;
+  height: 80%;
 }
 </style>
