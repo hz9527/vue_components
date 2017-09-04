a4135bf7a1da226568276b5053c6823bbf0508f6
huangzhong <huangzhong@daojia.com>
2017-09-04 18:51:03 +0800

    nothing

--- src/components/newPicker/index.vue
+++ src/components/newPicker/index.vue
@@ -13,6 +13,30 @@ export default {
         return []
       }
     }
+  },
+  data () {
+    return {
+      chooseIndex: [],
+      itemData: [],
+      _type: '' // normal tree
+    }
+  },
+  watch: {
+    list: {
+      immediate: true,
+      handler () {
+        this.initData()
+      }
+    }
+  },
+  methods: {
+    initData () {
+      this.list.map(item => {
+        if (item.data && item.data.constructor === Array) {
+          this._type = 'tree'
+        }
+      })
+    }
   }
 }
 </script>
