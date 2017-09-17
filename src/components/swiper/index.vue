f226becb48d725bdcfac72e1a1cf7b55edb71413
hz9527 <981379316@qq.com>
2017-09-17 12:00:59 +0800

    swiper

--- src/components/swiper/index.vue
+++ src/components/swiper/index.vue
@@ -1,10 +1,16 @@
 <template lang="html">
   <div class="swiper-con">
+    <slot name='head'></slot>
     <div class="content" ref='con' @touchstart='moveStart' @touchmove='move' @touchend='moveEnd' @touchcancel='moveEnd'>
       <div class="list-con" :style="conStyle">
         <slot name='item'></slot>
       </div>
+      <div class="index-con" v-show='showIndex' @click='chooseIndex'>
+        <div :data-index='i - 1' :class="['index-item', index === i - 1 ? 'active' : '']" v-for='i in itemLength' :key='i'></div>
+      </div>
+      <slot name='other'></slot>
     </div>
+    <slot name='foot'></slot>
   </div>
 </template>
 
@@ -14,11 +20,11 @@ export default {
   props: {
     autoTime: { // 自动滑动时间
       type: Number,
-      default: 0
+      default: 500
     },
     waitTime: { // 自动滑动等待时间
       type: Number,
-      default: 3000
+      default: 500
     },
     defaultIndex: {
       type: Number,
@@ -31,6 +37,10 @@ export default {
     showIndex: {
       type: Boolean,
       default: true
+    },
+    needUpdate: {
+      type: Boolean,
+      default: false
     }
   },
   data () {
@@ -40,7 +50,7 @@ export default {
       index: 0,
       _width: 0,
       _waitTick: null,
-      _autoTick: null,
+      // _autoTick: null,
       _revisdTick: null,
       _touch: null,
       _firstState: 0, // 0 normal 1 translate
@@ -57,10 +67,16 @@ export default {
     }
   },
   watch: {
-    defaultIndex: {
-      immediate: true,
-      handler (v) {
-        this.index = v
+    needUpdate (v) {
+      if (v) {
+        this._width = this.$refs.con.offsetWidth
+        this.initPage()
+        this.$emit('updated')
+      }
+    },
+    defaultIndex (v) {
+      if (v !== this.index) {
+        this.resetIndex(v)
       }
     },
     index (v) {
@@ -78,20 +94,20 @@ export default {
         if (!this._lastState && v > 0) {
           this._lastState = 1
           this.$slots.item[this.itemLength - 1].child.move(-this.itemLength * this._width)
-          console.log('moveLastItem')
+          // console.log('moveLastItem')
         } else if (this._lastState === 1 && v < -(this.itemLength - 2) * this._width) {
           this._lastState = 0
           this.$slots.item[this.itemLength - 1].child.move(0)
-          console.log('resetLastItem')
+          // console.log('resetLastItem')
         }
         if (this._firstState === 1 && v > -this._width) {
           this._firstState = 0
           this.$slots.item[0].child.move(0)
-          console.log('resetFirstItem')
+          // console.log('resetFirstItem')
         } else if (!this._firstState && v < -(this.itemLength - 1) * this._width) {
           this._firstState = 1
           this.$slots.item[0].child.move(this.itemLength * this._width)
-          console.log('moveFristItem')
+          // console.log('moveFristItem')
         }
       }
     }
@@ -119,13 +135,35 @@ export default {
       if (t > 300 || Math.abs(v) < this.speed) {
         v = this.speed * (s > 0 ? 1 : -1)
       }
-      this.revisd(v)
+      var move = this.translate % this._width
+      move = Math.abs(move)
+      var index
+      if ((v < 0 && move < this._width / 3) || (v > 0 && move > this._width * 2 / 3)) {
+        index = this.index
+      } else {
+        index = this.index + (v < 0 ? 1 : -1)
+        index = index < 0 ? this.itemLength - 1 : index >= this.itemLength ? 0 : index
+      }
+      this.revisd(v, index)
       this._touch = null
     },
-    revisd (v) {
-      var index = this.index + (v < 0 ? 1 : -1)
-      index = index < 0 ? this.itemLength - 1 : index >= this.itemLength ? 0 : index
+    chooseIndex (e) {
+      if ('index' in e.target.dataset) {
+        var index = Number(e.target.dataset.index)
+        if (index !== this.index) {
+          this.resetIndex(index)
+        }
+      }
+    },
+    revisd (v, index) {
       var translate = -index * this._width
+      if (Math.abs(translate - this.translate) >= this._width) {
+        if (this.translate > 0) {
+          this.translate -= this._width * this.itemLength
+        } else if (this.translate <= -this._width * (this.itemLength - 1)) {
+          this.translate += this._width * this.itemLength
+        }
+      }
       var step = v * Step
       var move = () => {
         this._revisdTick = setTimeout(() => {
@@ -137,25 +175,50 @@ export default {
             this.translate = translate
             this.index = index
             clearTimeout(this._revisdTick)
+            this._revisdTick = null
             // auto move
+            this.autoMove()
           }
         }, Step)
       }
       move()
     },
+    autoMove () {
+      if (this.autoTime > 0 && this.waitTime > 0 && this.itemLength > 1) {
+        this.stopAnimation()
+        this._waitTick = setTimeout(() => {
+          clearTimeout(this._waitTick)
+          this._waitTick = null
+          var index = this.index + 1
+          index = index < 0 ? this.itemLength - 1 : index >= this.itemLength ? 0 : index
+          this.revisd(-this._width / this.autoTime, index)
+        }, this.waitTime)
+      }
+    },
     stopAnimation () {
       if (this._waitTick) {
         clearTimeout(this._waitTick)
+        this._waitTick = null
       }
-      if (this._autoTick) {
-        clearTimeout(this._autoTick)
-      }
+      // if (this._autoTick) {
+      //   clearTimeout(this._autoTick)
+      // }
       if (this._revisdTick) {
         clearTimeout(this._revisdTick)
+        this._revisdTick = null
       }
     },
+    resetIndex (index) {
+      this.index = index
+      this.stopAnimation()
+      this.translate = -this._width * this.index
+      this.autoMove()
+    },
     initPage () {
-      this.itemLength = this.$slots.item.length
+      if (this.$slots.item) {
+        this.itemLength = this.$slots.item.length
+      }
+      this.resetIndex(this.defaultIndex)
     }
   },
   mounted () {
@@ -169,8 +232,26 @@ export default {
 .content {
   width: 100%;
   overflow: hidden;
+  position: relative;
 }
 .list-con {
   display: flex;
+}
+.index-con {
+  position: absolute;
+  bottom: 0.3rem;
+  left: 50%;
+  transform: translate(-50%, 0);
+  display: flex;
+  .index-item {
+    width: 0.1rem;
+    height: 0.1rem;
+    margin: 0 0.05rem;
+    border-radius: 50%;
+    background: rgba(255, 255, 255, 0.5);
+  }
+  .active {
+    background: #fff;
+  }
 }
 </style>
--- src/examples/swiper/index.vue
+++ src/examples/swiper/index.vue
@@ -3,7 +3,10 @@
     <div class="hz-head">
       <router-link to="/">&lt;  index</router-link>
     </div>
-    <swiper>
+    <swiper :defaultIndex='index' :waitTime='3000' :needUpdate='needUpdate' @updated='updated' @change='change'>
+      <div class="head" slot='head' @click='choose'>
+        <span :data-index='i' :class="[index === i ? 'active' : '']" v-for='(item, i) in list' :key='item.id'>{{item.text}}</span>
+      </div>
       <swiper-item v-for='item in list' :key='item.id' slot='item'>
         <div class="item" :style="{background: item.src}">{{item.text}}</div>
       </swiper-item>
@@ -13,12 +16,34 @@
 
 <script>
 import {list} from './data'
+// console.log(list)
 import Swiper from '../../components/swiper/index.vue'
 import SwiperItem from '../../components/swiper/src/swiperItem.vue'
 export default {
   data () {
     return {
-      list: list
+      list: [],
+      needUpdate: false,
+      index: 0
+    }
+  },
+  mounted () {
+    setTimeout(() => {
+      this.needUpdate = true
+      this.list = list.slice(0, 2)
+    }, 1000)
+  },
+  methods: {
+    choose (e) {
+      if ('index' in e.target.dataset) {
+        this.index = Number(e.target.dataset.index)
+      }
+    },
+    updated () {
+      this.needUpdate = false
+    },
+    change (v) {
+      this.index = v
     }
   },
   components: {
@@ -30,6 +55,21 @@ export default {
 
 <style lang="scss" scoped>
 @import '../common_style/common.scss';
+.head {
+  height: 0.3rem;
+  line-height: 0.3rem;
+  span {
+    display: inline-block;
+    height: 0.3rem;
+    padding: 0 0.1rem;
+    box-sizing: border-box;
+    color: #666;
+  }
+  .active {
+    border-bottom: 2px solid #f55;
+    color: #f55;
+  }
+}
 .item {
   height: 3rem;
   text-align: center;
